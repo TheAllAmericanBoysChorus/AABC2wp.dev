7199f09c105661d0f54e7a59ff9b13c275eefc3d
thegrid22593 <garrett.sanderson@gmail.com>
2015-08-27 21:14:48 -0700

    Many Changes

--- wp-content/themes/AABC/fall-gala.php
+++ wp-content/themes/AABC/fall-gala.php
@@ -12,17 +12,17 @@
 	</div>
 
 	<div class="small-12 large-centered columns text-center fall-gala-btn-row">
-		<button>Underwrite The Gala</button>
-		<button>Donate Auction Items</button>
-		<button>Purchase Gala Tickets</button>
-		<button>Bid In Online Auction</button>
+		<a href="<?php echo site_url();?>/uploads/dlm_uploads/2015/05/NewYorkNewYork_Underwritting_Packet_web.pdf"><button>Underwrite The Gala</button></a>
+		<a href="<?php echo site_url();?>/uploads/dlm_uploads/2015/05/NewYorkNewYork_Auction_Donation_Packet_web.pdf"><button>Donate Auction Items</button></a>
+		<a href="https://portal11.bidpal.net/Portal/bpe293867/main/home.html"><button>Purchase Gala Tickets</button>
+		<a href="#"><button>Bid In Online Auction<br/><em>(Opens October 7)</em></button></a>
 	</div>
 
-    <div class="small-12 large-8 columns right general-content-right">
+    <div class="small-12 large-8 columns left general-content-right">
         <?php dynamic_sidebar('fall-gala-main-content'); ?>
     </div>
 
-    <div class="small-12 large-4 columns left sidebar-left fall-gala-sidebar">
+    <div class="small-12 large-4 columns right sidebar-right fall-gala-sidebar">
         <?php dynamic_sidebar('fall-gala-sidebar'); ?>
     </div>
 </div>
--- wp-content/themes/AABC/header.php
+++ wp-content/themes/AABC/header.php
@@ -1,8 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" type="image/png" href="assets/images/aabc-favicon.png"/>
     <title><?php  ?></title>
