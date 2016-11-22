76d98ead2515b89ccbead1bc354566861c8085b1
Garrett Sanderson <garrett.sanderson@gmail.com>
2016-11-22 00:04:48 -0800

    edits alumni page

--- wp-content/themes/AABC/alumni-corner.php
+++ wp-content/themes/AABC/alumni-corner.php
@@ -14,12 +14,17 @@ get_header();
     </div>
 </div>
 
+<hr>
+
+<div class="row">
+    <div class="small-12 medium-10 large-7 small-centered text-center columns main-title">
+        <h1>Hallelujah Chorus</h1>
+        <a style="background: #242133; display: inline-block; color: #fff; margin-top: 1rem; padding: 0.5rem 1.5rem;" href="https://drive.google.com/open?id=0BytbGNc6KNy_WkdCMnFpd1hrVEU"><h5 style="color: white;">Download Files</h5></a>
+    </div>
+</div>
+
 <div class="row-full clearfix">
         <?php dynamic_sidebar('alumni-corner'); ?>
 </div>
 
 <?php get_footer(); ?>
-
-
-
-
